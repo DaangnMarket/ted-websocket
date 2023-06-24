dfc9731ca0151a872521115e6fd416205678feb8
Taerogrammer <hyung54321@naver.com>
2023-06-24 16:16:43 +0900

    reset xcode

---
+++ websocket-practice/websocket-practice/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  websocket-practice
+//
+//  Created by KimTaeHyung on 2023/06/24.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
